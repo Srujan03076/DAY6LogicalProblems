c183b2c9dc037a4ca059f4b5d421e2a56e0738b8
“Srujan03076” <srujanmahaja5@gmail.com>
2021-10-11 14:04:09 +0530

    Adding fibonnaci program

---
+++ fibonnaci.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+        namespace P_1_fibonacciSeries
+    {
+        class P_1_fibonacciSeries
+        {
+            static void Main(string[] args)
+            {
+                int num1 = 0, num2 = 1, num3, i, N;
+                Console.Write("Enter the N: ");
+
+
+                N = int.Parse(Console.ReadLine());
+                Console.Write(num1 + " " + num2 + " ");
+
+
+                for (i = 2; i < N; ++i)
+                {
+                    num3 = num1 + num2;
+                    Console.Write(num3 + " ");
+                    num1 = num2;
+                    num2 = num3;
+                }
+            }
+        }
+    }
+
+
